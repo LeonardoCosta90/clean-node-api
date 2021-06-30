d1cd58f1331e8b8752428d3fd0fc033b03f5c43a
Leonardo Costa <leonardoeverton.tec@gmail.com>
2021-06-30 11:15:34 -0300

    test: ensure SigUpControleer return 400 if an invalid email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,14 +1,27 @@
-import { HttpRequest, HttpResponse } from "../protocols/http"
-import { MissingParamError } from "../errors/missing-params-errors";
+import { HttpResponse, HttpRequest } from '../protocols/http'
+import { MissingParamError } from '../errors/missing-params-errors'
+import { InvalidParamError } from '../errors/invalid-params-errors'
 import { badRequest } from '../helpers/http-helper'
-import { Controller } from '../protocols/controller'
-export class SignUpController implements Controller {
-  handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+import {Controller } from '../protocols/controller'
+import { EmailValidator } from '../protocols/email-validator';
+
+export class SignUpController implements Controller{
+  private readonly emailValidator: EmailValidator
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
+  handle(httpRequest: HttpRequest): HttpResponse{
+    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation'];
+    for ( const field of requiredFields){
+      if(!httpRequest.body[field]){
+        return badRequest(new MissingParamError(field));
       }
     }
-  }
+    const isValid = this.emailValidator.isValid(httpRequest.body.email);
+    console.log(isValid)
+    if(!isValid){
+      return badRequest(new InvalidParamError('email'));
+    }
+  } 
 }
