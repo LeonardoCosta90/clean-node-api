15fdde5ce468784da39b78b8dd4c5bf829a9bf0b
Leonardo Costa <leonardoeverton.tec@gmail.com>
2021-07-04 15:57:00 -0300

    test: ensure LogControllerDecorator calls LogErrorRepository if controller returns a server error

--- src/main/routes/singup-routes.test.ts
+++ src/main/routes/singup-routes.test.ts
@@ -2,10 +2,11 @@ import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
-describe('SingUp Routes', () => {
+describe('SignUp Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
+
   afterAll(async () => {
     await MongoHelper.disconnect()
   })
@@ -14,12 +15,13 @@ describe('SingUp Routes', () => {
     const accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
+
   test('Should return an account on success', async () => {
     await request(app)
-      .post('/api/singup')
+      .post('/api/signup')
       .send({
         name: 'Leonardo',
-        email: 'leonardoeverton.tec@gmail.com',
+        email: 'leonardo.costa@gmail.com',
         password: '123',
         passwordConfirmation: '123'
       })
