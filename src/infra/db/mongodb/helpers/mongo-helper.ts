0650e1dadf77f4debcfd4a0537ab476d15c236d8
Leonardo Costa <leonardoeverton.tec@gmail.com>
2021-07-04 16:24:40 -0300

    feat: finish implementations

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,30 +1,30 @@
-import { Collection, MongoClient } from 'mongodb'
+import { MongoClient, Collection } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
   uri: null as string,
 
-  async connect (uri: string): Promise<void>{
+  async connect (uri: string): Promise<void> {
     this.uri = uri
     this.client = await MongoClient.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
   },
-  
-  async disconnect (): Promise<void>{
+
+  async disconnect (): Promise<void> {
     await this.client.close()
     this.client = null
   },
 
   async getCollection (name: string): Promise<Collection> {
-    if(!this.client?.isConnected()){
+    if (!this.client?.isConnected()) {
       await this.connect(this.uri)
-    }    
+    }
     return this.client.db().collection(name)
   },
 
-  map: (collection: any): any =>{
+  map: (collection: any): any => {
     const { _id, ...collectionWithoutId } = collection
     return Object.assign({}, collectionWithoutId, { id: _id })
   }
--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,4 +1,4 @@
-import { Express, Router } from 'express';
+import { Express, Router } from 'express'
 import fg from 'fast-glob'
 
 export default (app: Express): void => {
