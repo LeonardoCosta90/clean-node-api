e054d9df092e992d275f625ee35ae4ca89f2148c
Leonardo Costa <leonardoeverton.tec@gmail.com>
2021-07-07 22:16:55 -0300

    refactor: move required field validation to the ValidationComposite

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -5,7 +5,7 @@ import {
   EmailValidator,
   AddAccount,
 } from "./signup-protocols";
-import { InvalidParamError } from "../../errors";
+import { MissingParamError, InvalidParamError } from "../../errors";
 import { badRequest, serverError, ok } from "../../helpers/http-helper";
 import { Validation } from "../../helpers/validators/validation";
 
@@ -27,23 +27,25 @@ export class SignUpController implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const error = this.validation.validate(httpRequest.body);
-      if (error) {
-        return badRequest(error);
-      }
-
       const { name, email, password, passwordConfirmation } = httpRequest.body;
-      if (password !== passwordConfirmation) {
-        return badRequest(new InvalidParamError("passwordConfirmation"));
-      }
       const isValid = this.emailValidator.isValid(email);
-      if (!isValid) {
-        return badRequest(new InvalidParamError("email"));
-      }
       const account = await this.addAccount.add({
         name,
         email,
         password,
       });
+
+      if (error) {
+        return badRequest(error);
+      }
+
+      if (password !== passwordConfirmation) {
+        return badRequest(new InvalidParamError("passwordConfirmation"));
+      }
+
+      if (!isValid) {
+        return badRequest(new InvalidParamError("email"));
+      }
       return ok(account);
     } catch (error) {
       return serverError(error);
