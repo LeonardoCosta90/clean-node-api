33f9ba4f3f5bd6ea85a81fb6798a6a831c21cfd7
Leonardo Costa <leonardoeverton.tec@gmail.com>
2021-07-07 21:12:18 -0300

    feat: ensure LoginController returns 401 if invalid credentials are provided

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,8 +1,12 @@
-import { Controller, HttpRequest, HttpResponse } from "../../protocols";
-import { badRequest, serverError } from "../../helpers/http-helper";
-import { MissingParamError, InvalidParamError } from "../../errors";
-import { EmailValidator } from "../signup/signup-protocols";
 import { Authentication } from "../../../domain/usecases/authentication";
+import {
+  badRequest,
+  serverError,
+  unauthorized,
+} from "../../helpers/http-helper";
+import { Controller, HttpRequest, HttpResponse } from "../../protocols";
+import { EmailValidator } from "../signup/signup-protocols";
+import { MissingParamError, InvalidParamError } from "../../errors";
 
 export class LoginController implements Controller {
   private readonly emailValidator: EmailValidator;
@@ -16,18 +20,20 @@ export class LoginController implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const requiredFields = ["email", "password"];
-      const { email, password } = httpRequest.body;
-      const isValid = this.emailValidator.isValid(email);
-
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) {
           return badRequest(new MissingParamError(field));
         }
       }
-
-      if (!isValid) return badRequest(new InvalidParamError("email"));
-
-      await this.authentication.auth(email, password);
+      const { email, password } = httpRequest.body;
+      const isValid = this.emailValidator.isValid(email);
+      if (!isValid) {
+        return badRequest(new InvalidParamError("email"));
+      }
+      const accessToken = await this.authentication.auth(email, password);
+      if (!accessToken) {
+        return unauthorized();
+      }
     } catch (error) {
       return serverError(error);
     }
